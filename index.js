2d4351c3967e52bc6623323560ddb666c6f3cae9
Jose Quintero <ojosedo@gmail.com>
2022-05-19 15:57:02 -0500

    3.6

--- index.js
+++ index.js
@@ -4,53 +4,54 @@ const app = express()
 app.use(express.json())
 
 let phonebook = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
 ]
 
-const generateId = () => 
+const generateId = () =>
   Math.max(...phonebook.map(e => e.id)) + 1
 console.log('max id: ', generateId())
 
 app.get('/api/persons', (request, response) => {
-    response.json(phonebook)
+  response.json(phonebook)
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const entrybook = phonebook.find(n => n.id === id)
-    if (!entrybook) {
-        response.status(400).json({
-            error: 'content missing'
-        })
-    }
-    console.log('phone: ', String(entrybook.name))
-    response.json(entrybook)
+  const id = Number(request.params.id)
+  const entrybook = phonebook.find(n => n.id === id)
+  if (!entrybook) {
+    response.status(400).json({
+      error: 'content missing'
+    })
+    return
+  }
+  console.log('phone: ', String(entrybook.name))
+  response.json(entrybook)
 })
 
 app.get('/info', (request, response) => {
-    response.send(
-        `<h1>Phonebook has info for ${phonebook.length} people</h1>
+  response.send(
+    `<h1>Phonebook has info for ${phonebook.length} people</h1>
         <p>${new Date().toString()}</p>`
-    )
+  )
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -65,6 +66,20 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
   console.log(body)
+
+  if (!body.name || !body.number) {
+    response.status(400).json({
+      error: 'missing name and/or number'
+    })
+    return
+  }
+
+  if (phonebook.find(e => e.name === body.name)) {
+    response.status(400).json({
+      error: 'name must be inique'
+    })
+    return
+  }
 
   const newEntry = {
     id: generateId(),
