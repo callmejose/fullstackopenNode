c77d8201621fb5adbe87123ba1474908b20c5a42
Jose Quintero <ojosedo@gmail.com>
2022-05-19 15:01:39 -0500

    3.3

--- index.js
+++ index.js
@@ -28,6 +28,25 @@ app.get('/api/persons', (request, response) => {
     response.json(phonebook)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const entrybook = phonebook.find(n => n.id === id)
+    if (!entrybook) {
+        response.status(400).json({
+            error: 'content missing'
+        })
+    }
+    console.log('phone: ', String(entrybook.name))
+    response.json(entrybook)
+})
+
+app.get('/info', (request, response) => {
+    response.send(
+        `<h1>Phonebook has info for ${phonebook.length} people</h1>
+        <p>${new Date().toString()}</p>`
+    )
+})
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`app running on port ${PORT}`)
