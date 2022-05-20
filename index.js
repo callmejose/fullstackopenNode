aee33e38cdedd8a3ec785c4b5299282cf1be7e92
Jose Quintero <ojosedo@gmail.com>
2022-05-20 09:03:42 -0500

    3.7

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express")
+const morgan = require("morgan")
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let phonebook = [
   {
@@ -28,7 +31,7 @@ let phonebook = [
 
 const generateId = () =>
   Math.max(...phonebook.map(e => e.id)) + 1
-console.log('max id: ', generateId())
+// console.log('max id: ', generateId())
 
 app.get('/api/persons', (request, response) => {
   response.json(phonebook)
@@ -56,7 +59,7 @@ app.get('/info', (request, response) => {
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  console.log('delete', id)
+  // console.log('delete', id)
   phonebook = [...phonebook.filter(e => e.id !== id)]
   response.status(204).json({
     status: 'content deleted'
@@ -65,7 +68,7 @@ app.delete('/api/persons/:id', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
   const body = request.body
-  console.log(body)
+  // console.log(body)
 
   if (!body.name || !body.number) {
     response.status(400).json({
@@ -90,6 +93,12 @@ app.post('/api/persons', (request, response) => {
   phonebook = phonebook.concat(newEntry)
   response.status(201).json(newEntry)
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT)
