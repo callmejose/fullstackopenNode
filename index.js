2a6d6c03bf52487c995fb6fbddad5bfc711f5cf8
Jose Quintero <ojosedo@gmail.com>
2022-05-23 16:51:38 -0500

    build static page

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 
 const app = express()
 
@@ -26,6 +27,8 @@ let phonebook = [
   }
 ]
 
+app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
 
 morgan.token('newEntry', (req) => {
@@ -110,6 +113,6 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT)
 console.log(`app running on port ${PORT}`)
